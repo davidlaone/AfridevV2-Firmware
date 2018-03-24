85ee9186dfc83a05d8a1dec4b2c26eb1f9d81ef7
David Laone <david.laone@mindinspire.com>
2018-03-24 15:47:28 -0700

    Create new CCS project to support water debug. This is a separate project that creates links to files in the AfridevV2_MSP430/src folder. This project is used to support IPS uart output for their water debug.

---
+++ AfridevV2_MSP430_Water_Debug/src_local/debugUart.h
@@ -0,0 +1,50 @@
+/*
+ * debugUart.h
+ *
+ *  Created on: Nov 17, 2016
+ *      Author: robl
+ */
+
+#ifndef SRC_LOCAL_DEBUGUART_H_
+#define SRC_LOCAL_DEBUGUART_H_
+
+#define DEBUG_LINE_SIZE 72
+/**
+ * \def ISR_BUF_SIZE
+ * \brief Define the size of the UART receive buffer.
+ */
+#define ISR_BUF_SIZE ((uint8_t)200)
+
+#define DBG_DETAILS
+//#define DBG_SAMPLES
+#define DBG_TEMP
+#define DBG_POUR
+
+extern uint8_t dbg_line[DEBUG_LINE_SIZE];
+
+extern void dbg_uart_init(void);
+extern void dbg_uart_write(uint8_t *writeCmdP, uint8_t len);
+extern uint8_t dbg_uart_txqempty(void);
+extern uint8_t dbg_uart_txpend(void);
+extern uint8_t dbg_uart_read(void);
+
+#ifdef DBG_DETAILS
+void debug_padSummary(uint32_t sys_time, uint8_t level, uint8_t unknowns, uint8_t pump_active);
+void debug_sampProgress(void);
+void debug_padTargets(void);
+#endif
+
+#ifdef DBG_SAMPLES
+void debug_sample_dump(void);
+#endif
+
+#ifdef DBG_TEMP
+void debug_internalTemp(uint32_t sys_time, uint16_t temp);
+#endif
+
+#ifdef DBG_POUR
+void debug_pour_total(uint32_t sys_time, uint32_t total_pour);
+#endif
+
+
+#endif /* SRC_LOCAL_DEBUGUART_H_ */
